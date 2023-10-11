e7c33e6c20cca04bef26e574b3e69fd4d6656e29
Pwaps <pigny.antoine@gmail.com>
2023-10-11 13:38:59 +0200

    adds fastify adapter

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import 'dotenv/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
   const PORT = process.env.APPLICATION_DEFAULT_PORT;
   const BASE_URI = process.env.APPLICATION_DEFAULT_URL;
   await app.listen(PORT, BASE_URI);
